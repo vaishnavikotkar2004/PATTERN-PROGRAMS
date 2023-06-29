ba17735e76c9bc5d5c921c5499d203cbc5ba9014
Vaishnavi Kotkar <129155876+vaishnavikotkar2004@users.noreply.github.com>
2023-06-29 10:43:42 +0530

    Add files via upload

---
+++ pattern_8.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int i,j;
+  for(i=5;i>=1;i--)
+  {
+   for(j=5;j>=i;j--)
+   {
+    printf("%d",j);
+   }
+   printf("\n");
+  }
+}
+
+output:-5
+        5 4
+        5 4 3
+        5 4 3 2
+        5 4 3 2 1

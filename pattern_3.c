53e12fff0d6c48e50ebed0ca786f8f70ceedc499
Vaishnavi Kotkar <129155876+VAISHNAVI1018@users.noreply.github.com>
2023-06-29 10:09:17 +0530

    Add files via upload

---
+++ pattern_3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int i,j;
+  for(i=1;i<=10;i++)
+  {
+    for(j=1;j<=10;j++)
+    {
+      printf("%d\t",i*j);
+    }
+  printf("\n");
+  }
+}
+
+output:-1 to 10 tables display. 

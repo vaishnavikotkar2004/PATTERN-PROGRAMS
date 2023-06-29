3d24b6599e6155374db34dd2de30531613d664c6
Vaishnavi Kotkar <129155876+VAISHNAVI1018@users.noreply.github.com>
2023-06-29 10:19:34 +0530

    Add files via upload

---
+++ pattern_6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int i,j;
+ for(i=1;i<=10;i++)
+ {
+   for(j=1;j<=10;j++)
+   {
+      printf("*");
+   }
+   printf("\n");
+ }
+}
+
+output:- 10 times stars(*) print in rows & columns.
